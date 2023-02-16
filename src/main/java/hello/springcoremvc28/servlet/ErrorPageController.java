d310713997d108b78cfab4b070fc4c56b26beef0
MinyShrimp <ksk7584@gmail.com>
2023-02-16 23:46:15 +0900

    8강: 예외 처리와 오류 페이지 - 서블릿 예외 처리 - 오류 페이지 작동 원리 - RequestDispatcher

--- src/main/java/hello/springcoremvc28/servlet/ErrorPageController.java
+++ src/main/java/hello/springcoremvc28/servlet/ErrorPageController.java
@@ -1,5 +1,6 @@
 package hello.springcoremvc28.servlet;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -10,12 +11,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/error-page")
 public class ErrorPageController {
+    private void printErrorInfo(
+            HttpServletRequest req
+    ) {
+        // 예외
+        log.info("ERROR_EXCEPTION: ex = {}", req.getAttribute(RequestDispatcher.ERROR_EXCEPTION));
+        // 예외 타입
+        log.info("ERROR_EXCEPTION_TYPE: {}", req.getAttribute(RequestDispatcher.ERROR_EXCEPTION_TYPE));
+        // 예외 메세지
+        log.info("ERROR_MESSAGE: {}", req.getAttribute(RequestDispatcher.ERROR_MESSAGE));
+        // 오류가 발생한 클라이언트 요청 URI
+        log.info("ERROR_REQUEST_URI: {}", req.getAttribute(RequestDispatcher.ERROR_REQUEST_URI));
+        // 오류가 발생한 서블릿 이름
+        log.info("ERROR_SERVLET_NAME: {}", req.getAttribute(RequestDispatcher.ERROR_SERVLET_NAME));
+        // 오류가 발생한 HTTP 상태 코드
+        log.info("ERROR_STATUS_CODE: {}", req.getAttribute(RequestDispatcher.ERROR_STATUS_CODE));
+
+        log.info("dispatchType: {}", req.getDispatcherType());
+    }
+
     @RequestMapping("/404")
     public String errorPage404(
             HttpServletRequest req,
             HttpServletResponse resp
     ) {
         log.info("GET /error-page/404");
+        printErrorInfo(req);
         return "error-page/404";
     }
 
@@ -25,6 +46,7 @@ public class ErrorPageController {
             HttpServletResponse resp
     ) {
         log.info("GET /error-page/500");
+        printErrorInfo(req);
         return "error-page/500";
     }
 }
